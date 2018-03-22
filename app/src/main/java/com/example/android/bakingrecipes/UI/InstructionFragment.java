3ba4a74aadc5ce24240f757cb04d87026f9f922a
Silky <silky.ng@gmail.com>
2018-03-22 08:54:49 +0000

    made video fragment class

---
+++ app/src/main/java/com/example/android/bakingrecipes/UI/InstructionFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.bakingrecipes.UI;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Silky on 22/03/2018.
+ */
+
+public class InstructionFragment extends Fragment {
+}
--- app/src/main/java/com/example/android/bakingrecipes/UI/VideoFragment.java
+++ app/src/main/java/com/example/android/bakingrecipes/UI/VideoFragment.java
@@ -1,11 +1,14 @@
 package com.example.android.bakingrecipes.UI;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.android.bakingrecipes.R;
 
 /**This video fragment will be used in the master detail flow layout for tablets in landscape mode.
  * The video will display the recipe step and will be replaced by a new one when the user clicks on a
@@ -15,11 +18,39 @@ import android.view.ViewGroup;
 
 public class VideoFragment extends Fragment {
 
+    private String videoURL;
+    private com.devbrackets.android.exomedia.ui.widget.VideoView videoView;
+    public static final String VIDEO_URL = "video_url";
+
     public VideoFragment(){}
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View rootView = inflater.inflate(R.layout.video_fragment_layout, container, false);
+
+        if(savedInstanceState != null){
+            videoURL = savedInstanceState.getString(VIDEO_URL);
+        }
+        videoView = rootView.findViewById(R.id.video_view_fragment);
+        videoView.setVideoURI(Uri.parse(videoURL));
+        videoView.start();
+        return rootView;
+    }
+
+    public void setUpVideo(String videoURL){
+        this.videoURL=videoURL;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        videoView.release();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle currentState) {
+        currentState.putString(VIDEO_URL, videoURL);
     }
 }
