cbba8696992e380d1f903257482bac9ea9746361
Silky <silky.ng@gmail.com>
2018-03-24 08:53:28 +0000

    made fragment handle video URLs

--- app/src/main/java/com/example/android/bakingrecipes/UI/VideoFragment.java
+++ app/src/main/java/com/example/android/bakingrecipes/UI/VideoFragment.java
@@ -1,6 +1,6 @@
 package com.example.android.bakingrecipes.UI;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
