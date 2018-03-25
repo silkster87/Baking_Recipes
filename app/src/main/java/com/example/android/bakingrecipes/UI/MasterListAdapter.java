c30a907f52e0d9cdfe403e88be6fcc0d47b6b82f
Silky <silky.ng@gmail.com>
2018-03-25 18:30:27 +0100

    added feature to change color of selected step

--- app/src/main/java/com/example/android/bakingrecipes/UI/MasterListAdapter.java
+++ app/src/main/java/com/example/android/bakingrecipes/UI/MasterListAdapter.java
@@ -1,11 +1,13 @@
 package com.example.android.bakingrecipes.UI;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.android.bakingrecipes.R;
@@ -25,10 +27,12 @@ public class MasterListAdapter extends RecyclerView.Adapter<MasterListAdapter.Vi
 
     private final OnItemClickListener listener;
     private List<Step> listOfSteps;
+    private int row_index = 0;
 
-    public MasterListAdapter(List<Step> listOfSteps, OnItemClickListener listener){
+    public MasterListAdapter(List<Step> listOfSteps, int stepNumber, OnItemClickListener listener){
         this.listOfSteps=listOfSteps;
         this.listener=listener;
+        this.row_index=stepNumber;
     }
 
     @NonNull
@@ -38,15 +42,18 @@ public class MasterListAdapter extends RecyclerView.Adapter<MasterListAdapter.Vi
         int layoutIdForListItem = R.layout.recipe_step_item;
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(layoutIdForListItem, parent, false);
-        MasterListAdapter.ViewHolder viewHolder = new MasterListAdapter.ViewHolder(view);
-        return viewHolder;
+        return new MasterListAdapter.ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MasterListAdapter.ViewHolder holder, int position) {
-        holder.bind(listOfSteps.get(position), listener);
-        holder.mRecipeItemTextView.setText(listOfSteps.get(position).getmShortDesc());
-
+    public void onBindViewHolder(@NonNull final MasterListAdapter.ViewHolder holder, final int position) {
+        holder.bind(listOfSteps.get(position), listener, position);
+        if(position!=0) {
+            String recipeStep = Integer.toString(listOfSteps.get(position).getmID()) + ". " + listOfSteps.get(position).getmShortDesc();
+            holder.mRecipeItemTextView.setText(recipeStep);
+        } else {
+            holder.mRecipeItemTextView.setText(listOfSteps.get(position).getmShortDesc());
+        }
     }
 
     @Override
@@ -54,7 +61,6 @@ public class MasterListAdapter extends RecyclerView.Adapter<MasterListAdapter.Vi
         if(listOfSteps==null)return 0;
         return listOfSteps.size();
     }
-
 
     public interface OnItemClickListener {
         void onItemClick(Step recipeStep);
@@ -64,19 +70,30 @@ public class MasterListAdapter extends RecyclerView.Adapter<MasterListAdapter.Vi
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public final TextView mRecipeItemTextView;
+        public final LinearLayout mLinearLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            mLinearLayout = itemView.findViewById(R.id.list_item_linear_layout);
             mRecipeItemTextView = itemView.findViewById(R.id.recipe_item);
         }
 
-        public void bind(final Step step, final OnItemClickListener listener) {
+        public void bind(final Step step, final OnItemClickListener listener, final int position) {
             itemView.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View view) {
                     listener.onItemClick(step);
+                    row_index = position;
+                    notifyDataSetChanged();
                 }
             });
+            if(row_index==position){
+                mLinearLayout.setBackgroundColor(Color.parseColor("#b2ebf2"));
+            }
+            else {
+                mLinearLayout.setBackgroundColor(Color.parseColor("#ffffff"));
+            }
         }
     }
+
 }
