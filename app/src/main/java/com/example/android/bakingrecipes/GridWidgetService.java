b34a871b6a77c03b0a17f8c02af39cc928118268
Silky <silky.ng@gmail.com>
2018-03-30 16:23:12 +0100

    added widget provider and service

--- app/src/main/java/com/example/android/bakingrecipes/GridWidgetService.java
+++ app/src/main/java/com/example/android/bakingrecipes/GridWidgetService.java
@@ -1,4 +1,118 @@
 package com.example.android.bakingrecipes;
 
-public class GridWidgetService {
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.example.android.bakingrecipes.provider.RecipeContract;
+
+public class GridWidgetService extends RemoteViewsService{
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new GridRemoteViewsFactory(this.getApplicationContext());
+    }
+
+
 }
+ class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    Context mContext;
+    Cursor mCursor;
+
+    public GridRemoteViewsFactory(Context applicationContext) {
+        mContext=applicationContext;
+    }
+
+     @Override
+     public void onCreate() {
+
+     }
+
+     //This method is called on start
+     //and when notifyAppWidgetViewDataChanged() is called
+     @Override
+     public void onDataSetChanged() {
+        //Get all favourited recipes
+         Uri RECIPE_URI = RecipeContract.RecipeEntry.CONTENT_URI;
+         if(mCursor != null) mCursor.close();
+         mCursor = mContext.getContentResolver().query(
+                 RECIPE_URI,
+                 null,
+                 null,
+                 null,
+                 null
+         );
+
+     }
+
+     @Override
+     public void onDestroy() {
+        mCursor.close();
+     }
+
+     @Override
+     public int getCount() {
+        if(mCursor==null) return 0;
+        return mCursor.getCount();
+     }
+
+     @Override
+     public RemoteViews getViewAt(int position) {
+        if( mCursor==null || mCursor.getCount() == 0) return null;
+        mCursor.moveToPosition(position);
+        int recipeNameIndex = mCursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_RECIPE_NAME);
+        int servingsIndex = mCursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_NO_OF_SERVINGS);
+        int ingredientsIndex = mCursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_INGREDIENTS);
+
+        String recipeName = mCursor.getString(recipeNameIndex);
+        int noOfServings = mCursor.getInt(servingsIndex);
+        String ingredients = mCursor.getString(ingredientsIndex);
+
+        String servings = "Servings: " + Integer.toString(noOfServings);
+
+        RemoteViews views = new RemoteViews(mContext.getPackageName(),
+                R.layout.recipe_widget_provider);
+
+        views.setTextViewText(R.id.widget_recipe_name, recipeName);
+        views.setTextViewText(R.id.widget_servings, servings);
+        views.setTextViewText(R.id.appwidget_text, ingredients);
+
+        //Need to make an onClick fill in intent for each recipe item
+         //When user clicks on an item it will open up Recipe Detail Activity and use the recipe ID
+         //to make a query and find the Recipe object by its ID
+         Bundle extras = new Bundle();
+
+         int recipeIDIndex = mCursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_RECIPE_ID);
+         int recipe_ID = mCursor.getInt(recipeIDIndex);
+         extras.putInt(RecipeContract.RecipeEntry.COLUMN_RECIPE_ID, recipe_ID);
+         Intent fillInIntent = new Intent();
+         fillInIntent.putExtras(extras);
+         views.setOnClickFillInIntent(R.id.recipe_widget, fillInIntent);
+
+         return views;
+     }
+
+     @Override
+     public RemoteViews getLoadingView() {
+         return null;
+     }
+
+     @Override
+     public int getViewTypeCount() {
+         return 0;
+     }
+
+     @Override
+     public long getItemId(int i) {
+         return 0;
+     }
+
+     @Override
+     public boolean hasStableIds() {
+         return false;
+     }
+ }
--- app/src/main/java/com/example/android/bakingrecipes/RecipeWidgetProvider.java
+++ app/src/main/java/com/example/android/bakingrecipes/RecipeWidgetProvider.java
@@ -1,23 +1,85 @@
 package com.example.android.bakingrecipes;
 
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.widget.RemoteViews;
+
+import com.example.android.bakingrecipes.provider.RecipeContract;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class RecipeWidgetProvider extends AppWidgetProvider {
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId, int recipeID, String recipeName, int noOfServings, String ingredients) {
+
+        //Get current width to decide whether to use single or grid view
+        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+        int width = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        RemoteViews rv;
+        if (width < 300) {
+            rv = getSingleRecipeRemoteView(context, recipeID, recipeName, noOfServings, ingredients);
+        } else {
+            rv = getGridRecipeRemoteView(context);
+        }
+
+        appWidgetManager.updateAppWidget(appWidgetId, rv);
+    }
+
+
+    private static RemoteViews getSingleRecipeRemoteView(Context context, int recipeID, String recipeName, int noOfServings, String ingredients) {
+        Intent intent;
+        if(recipeID == RecipeContract.INVALID_RECIPE_ID){
+            intent = new Intent(context, MainActivity.class);
+        }else{
+            intent = new Intent(context, RecipeDetailActivity.class);
+            intent.putExtra(RecipeContract.EXTRA_RECIPE_ID, recipeID);
+        }
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        String servings = "Servings: " + Integer.toString(noOfServings);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
+        views.setTextViewText(R.id.widget_recipe_name, recipeName);
+        views.setTextViewText(R.id.widget_servings, servings);
+        views.setTextViewText(R.id.appwidget_text, ingredients);
+
+        views.setOnClickPendingIntent(R.id.recipe_widget, pendingIntent);
+
+        return views;
+
+    }
+
+
+    private static RemoteViews getGridRecipeRemoteView(Context context){
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_grid_view);
+
+        Intent intent = new Intent(context, GridWidgetService.class);
+        views.setRemoteAdapter(R.id.widget_grid_view, intent);
+
+        Intent appIntent = new Intent(context, RecipeDetailActivity.class);
+        PendingIntent appPendingIntent = PendingIntent.getActivity(context,0,appIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setPendingIntentTemplate(R.id.widget_grid_view, appPendingIntent);
+
+        views.setEmptyView(R.id.widget_grid_view, R.id.empty_view);
+        return views;
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+        RecipeWidgetUpdateService.startActionUpdateRecipeWidgets(context);
     }
-
 
     @Override
     public void onEnabled(Context context) {
@@ -29,16 +91,6 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-            int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
 }
 
