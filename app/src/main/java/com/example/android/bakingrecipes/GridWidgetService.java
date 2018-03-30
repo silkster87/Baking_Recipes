925591f2ca6f4e60cfb28ce99848b2ff3f43e4a4
Silky <silky.ng@gmail.com>
2018-03-30 16:22:13 +0100

    added widget service

---
+++ app/src/main/java/com/example/android/bakingrecipes/GridWidgetService.java
@@ -0,0 +1,4 @@
+package com.example.android.bakingrecipes;
+
+public class GridWidgetService {
+}
---
+++ app/src/main/java/com/example/android/bakingrecipes/provider/RecipeContentProvider.java
@@ -0,0 +1,6 @@
+package com.example.android.bakingrecipes.provider;
+
+import android.content.ContentProvider;
+
+public class RecipeContentProvider extends ContentProvider {
+}
