a83edafaf9947172fb3239146a87ed21540bf43f
Silky <silky.ng@gmail.com>
2018-03-31 18:21:00 +0100

    make videoplayer seek to position

--- app/src/main/java/com/example/android/bakingrecipes/UI/VideoFragment.java
+++ app/src/main/java/com/example/android/bakingrecipes/UI/VideoFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.devbrackets.android.exomedia.ui.widget.VideoControls;
 import com.example.android.bakingrecipes.R;
 
 /**This video fragment will be used in the master detail flow layout for tablets in landscape mode.
@@ -21,6 +22,7 @@ public class VideoFragment extends Fragment {
     private String videoURL;
     private com.devbrackets.android.exomedia.ui.widget.VideoView videoView;
     public static final String VIDEO_URL = "video_url";
+    private static final String VIDEO_POSITION = "video_position";
 
 
     public VideoFragment(){}
@@ -30,12 +32,19 @@ public class VideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.video_fragment_layout, container, false);
+        long currentPosition = 0;
 
         if(savedInstanceState != null){
             videoURL = savedInstanceState.getString(VIDEO_URL);
+            currentPosition = savedInstanceState.getLong(VIDEO_POSITION);
         }
         videoView = rootView.findViewById(R.id.video_view_fragment);
         videoView.setVideoURI(Uri.parse(videoURL));
+
+        VideoControls controls = videoView.getVideoControls();
+        controls.setRewindButtonEnabled(true);
+
+        videoView.seekTo(currentPosition);
         videoView.start();
         return rootView;
     }
@@ -53,7 +62,22 @@ public class VideoFragment extends Fragment {
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+        videoView.pause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        videoView.start();
+    }
+
+    @Override
     public void onSaveInstanceState(Bundle currentState) {
         currentState.putString(VIDEO_URL, videoURL);
+        long videoCurrentPosition = videoView.getCurrentPosition();
+
+        currentState.putLong(VIDEO_POSITION, videoCurrentPosition);
     }
 }
