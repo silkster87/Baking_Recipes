102e60ac9b91e46754e0c8c728e6118ddf6743fd
Silky <silky.ng@gmail.com>
2018-03-31 16:39:08 +0100

    changed to use shared preferences

--- app/src/main/java/com/example/android/bakingrecipes/RecipeWidgetProvider.java
+++ app/src/main/java/com/example/android/bakingrecipes/RecipeWidgetProvider.java
@@ -1,83 +1,41 @@
 package com.example.android.bakingrecipes;
 
-import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
+
 import android.widget.RemoteViews;
 
-import com.example.android.bakingrecipes.provider.RecipeContract;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class RecipeWidgetProvider extends AppWidgetProvider {
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId, int recipeID, String recipeName, int noOfServings, String ingredients) {
 
-        //Get current width to decide whether to use single or grid view
-        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
-        int width = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
-        RemoteViews rv;
-        if (width < 300) {
-            rv = getSingleRecipeRemoteView(context, recipeID, recipeName, noOfServings, ingredients);
-        } else {
-            rv = getGridRecipeRemoteView(context);
-        }
+    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId){
 
-        appWidgetManager.updateAppWidget(appWidgetId, rv);
-    }
-
-
-    private static RemoteViews getSingleRecipeRemoteView(Context context, int recipeID, String recipeName, int noOfServings, String ingredients) {
-        Intent intent;
-        if(recipeID == RecipeContract.INVALID_RECIPE_ID){
-            intent = new Intent(context, MainActivity.class);
-        }else{
-            intent = new Intent(context, RecipeDetailActivity.class);
-            intent.putExtra(RecipeContract.EXTRA_RECIPE_ID, recipeID);
-        }
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        String servings = "Servings: " + Integer.toString(noOfServings);
-
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
-        views.setTextViewText(R.id.widget_recipe_name, recipeName);
-        views.setTextViewText(R.id.widget_servings, servings);
-        views.setTextViewText(R.id.appwidget_text, ingredients);
-
-        views.setOnClickPendingIntent(R.id.recipe_widget, pendingIntent);
-
-        return views;
-
-    }
-
-
-    private static RemoteViews getGridRecipeRemoteView(Context context){
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_grid_view);
+        RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_grid_view);
 
         Intent intent = new Intent(context, GridWidgetService.class);
-        views.setRemoteAdapter(R.id.widget_grid_view, intent);
+        rv.setRemoteAdapter(R.id.widget_grid_view, intent);
 
+        //Set the RecipeDetailActivity intent to launch when clicked
         Intent appIntent = new Intent(context, RecipeDetailActivity.class);
-        PendingIntent appPendingIntent = PendingIntent.getActivity(context,0,appIntent,PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setPendingIntentTemplate(R.id.widget_grid_view, appPendingIntent);
+        PendingIntent appPendingIntent = PendingIntent.getActivity(context, PendingIntent.FLAG_UPDATE_CURRENT
+                ,appIntent, 0);
+        rv.setPendingIntentTemplate(R.id.widget_grid_view, appPendingIntent);
 
-        views.setEmptyView(R.id.widget_grid_view, R.id.empty_view);
-        return views;
+        //Set to empty view if no recipes are added to widgets
+        rv.setEmptyView(R.id.widget_grid_view, R.id.empty_view);
+        appWidgetManager.updateAppWidget(appWidgetId, rv);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
+        //Start the intent service update widget action, the service takes care of updating the widgets UI
         RecipeWidgetUpdateService.startActionUpdateRecipeWidgets(context);
     }
 
@@ -92,5 +50,10 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     }
 
 
+    public static void updateRecipeWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for(int appWidgetId : appWidgetIds){
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
 }
 
