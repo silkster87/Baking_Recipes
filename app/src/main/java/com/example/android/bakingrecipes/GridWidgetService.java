e80cec773f4aadc6fa4c1f7cac3862caf7e7abe4
Silky <silky.ng@gmail.com>
2018-03-31 16:38:01 +0100

    changed to use shared preferences

--- app/src/main/java/com/example/android/bakingrecipes/GridWidgetService.java
+++ app/src/main/java/com/example/android/bakingrecipes/GridWidgetService.java
@@ -2,13 +2,20 @@ package com.example.android.bakingrecipes;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
-import com.example.android.bakingrecipes.provider.RecipeContract;
+import com.example.android.bakingrecipes.RecipeObjects.Ingredient;
+import com.example.android.bakingrecipes.RecipeObjects.Recipe;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class GridWidgetService extends RemoteViewsService{
     @Override
@@ -20,80 +27,78 @@ public class GridWidgetService extends RemoteViewsService{
 }
  class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
 
-    Context mContext;
-    Cursor mCursor;
+    private Context mContext;
+    private Map<String, ?> mKeys;
 
     public GridRemoteViewsFactory(Context applicationContext) {
         mContext=applicationContext;
     }
 
      @Override
-     public void onCreate() {
-
-     }
+     public void onCreate() { }
 
      //This method is called on start
      //and when notifyAppWidgetViewDataChanged() is called
      @Override
      public void onDataSetChanged() {
-        //Get all favourited recipes
-         Uri RECIPE_URI = RecipeContract.RecipeEntry.CONTENT_URI;
-         if(mCursor != null) mCursor.close();
-         mCursor = mContext.getContentResolver().query(
-                 RECIPE_URI,
-                 null,
-                 null,
-                 null,
-                 null
-         );
-
+        //Get all favourited recipes from shared preferences
+         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(mContext.getApplicationContext());
+         mKeys = sharedPrefs.getAll();
      }
 
      @Override
-     public void onDestroy() {
-        mCursor.close();
-     }
+     public void onDestroy() { }
 
      @Override
      public int getCount() {
-        if(mCursor==null) return 0;
-        return mCursor.getCount();
+        if(mKeys==null) return 0;
+        return mKeys.size();
      }
 
      @Override
      public RemoteViews getViewAt(int position) {
-        if( mCursor==null || mCursor.getCount() == 0) return null;
-        mCursor.moveToPosition(position);
-        int recipeNameIndex = mCursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_RECIPE_NAME);
-        int servingsIndex = mCursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_NO_OF_SERVINGS);
-        int ingredientsIndex = mCursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_INGREDIENTS);
 
-        String recipeName = mCursor.getString(recipeNameIndex);
-        int noOfServings = mCursor.getInt(servingsIndex);
-        String ingredients = mCursor.getString(ingredientsIndex);
 
-        String servings = "Servings: " + Integer.toString(noOfServings);
+         RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.recipe_widget_provider);
 
-        RemoteViews views = new RemoteViews(mContext.getPackageName(),
-                R.layout.recipe_widget_provider);
+         for(Map.Entry<String, ?> entry : mKeys.entrySet()){
+            String json = entry.getValue().toString();
+            Gson gson = new Gson();
+            Recipe mRecipe = gson.fromJson(json, Recipe.class);
 
-        views.setTextViewText(R.id.widget_recipe_name, recipeName);
-        views.setTextViewText(R.id.widget_servings, servings);
-        views.setTextViewText(R.id.appwidget_text, ingredients);
+            String recipeName = mRecipe.getRecipeName();
+            int noOfServings = mRecipe.getmServings();
+            ArrayList<Ingredient> mIngredientList = mRecipe.getArrayOfIngredients();
 
-        //Need to make an onClick fill in intent for each recipe item
-         //When user clicks on an item it will open up Recipe Detail Activity and use the recipe ID
-         //to make a query and find the Recipe object by its ID
-         Bundle extras = new Bundle();
+            String servings = "Servings: " + Integer.toString(noOfServings);
 
-         int recipeIDIndex = mCursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_RECIPE_ID);
-         int recipe_ID = mCursor.getInt(recipeIDIndex);
-         extras.putInt(RecipeContract.RecipeEntry.COLUMN_RECIPE_ID, recipe_ID);
-         Intent fillInIntent = new Intent();
-         fillInIntent.putExtras(extras);
-         views.setOnClickFillInIntent(R.id.recipe_widget, fillInIntent);
+            StringBuilder builder = new StringBuilder();
 
-         return views;
+            for(int i=0; i<mIngredientList.size(); i++){
+
+                builder.append(mIngredientList.get(i).getIngredient())
+                        .append(", ")
+                        .append(mIngredientList.get(i).getmQuantity())
+                        .append(" ")
+                        .append((mIngredientList.get(i).getmMeasure()))
+                        .append("\n");
+            }
+
+            String ingredients = builder.toString();
+
+            views.setTextViewText(R.id.widget_recipe_name, recipeName);
+            views.setTextViewText(R.id.widget_servings, servings);
+            views.setTextViewText(R.id.appwidget_text, ingredients);
+
+            //Fill in the onClick PendingIntent template using specific recipe for each item
+            Bundle extras = new Bundle();
+            extras.putParcelable(MainActivity.recipeBundle, mRecipe);
+            Intent fillInIntent = new Intent();
+            fillInIntent.putExtras(extras);
+            views.setOnClickFillInIntent(R.id.recipe_widget, fillInIntent);
+
+        }
+        return views;
      }
 
      @Override
@@ -103,16 +108,16 @@ public class GridWidgetService extends RemoteViewsService{
 
      @Override
      public int getViewTypeCount() {
-         return 0;
+         return 1;
      }
 
      @Override
      public long getItemId(int i) {
-         return 0;
+         return i;
      }
 
      @Override
      public boolean hasStableIds() {
-         return false;
+         return true;
      }
  }
