e0109084a7291ff1bca61b968959ab02507a9516
Silky <silky.ng@gmail.com>
2018-04-07 19:30:29 +0100

    persist playback

--- app/src/main/java/com/example/android/bakingrecipes/UI/VideoFragment.java
+++ app/src/main/java/com/example/android/bakingrecipes/UI/VideoFragment.java
@@ -10,15 +10,20 @@ import android.view.ViewGroup;
 
 import com.example.android.bakingrecipes.R;
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
@@ -34,12 +39,14 @@ import com.google.android.exoplayer2.util.Util;
  * Created by Silky on 20/03/2018.
  */
 
-public class VideoFragment extends Fragment {
+public class VideoFragment extends Fragment implements ExoPlayer.EventListener {
 
     private String videoURL;
     private SimpleExoPlayerView videoView;
     private SimpleExoPlayer player;
-    private Long videoPosition;
+    private Long videoPosition = null;
+    private Boolean getPlayWhenReady = true;
+    private String PLAY_STATE = "Play_State";
     public static final String VIDEO_URL = "video_url";
     private static final String POSITION = "position";
 
@@ -58,6 +65,8 @@ public class VideoFragment extends Fragment {
             videoURL = savedInstanceState.getString(VIDEO_URL);
             if(savedInstanceState.containsKey(POSITION))
                 videoPosition = savedInstanceState.getLong(POSITION);
+            if(savedInstanceState.containsKey(PLAY_STATE))
+                getPlayWhenReady = savedInstanceState.getBoolean(PLAY_STATE);
         }
 
         videoView = rootView.findViewById(R.id.video_view_fragment);
@@ -88,11 +97,11 @@ public class VideoFragment extends Fragment {
             videoView.requestFocus();
             if (videoPosition != null) {
                 player.prepare(videoSource, false, true);
-                player.setPlayWhenReady(true);
+                player.setPlayWhenReady(getPlayWhenReady);
                 player.seekTo(videoPosition);
             } else {
                 player.prepare(videoSource);
-                player.setPlayWhenReady(true);
+                player.setPlayWhenReady(getPlayWhenReady);
             }
             videoView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIXED_HEIGHT);
         }
@@ -106,6 +115,7 @@ public class VideoFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
          if(player!=null){
+             videoPosition = player.getCurrentPosition();
              player.stop();
              player.release();
              player=null;
@@ -117,28 +127,48 @@ public class VideoFragment extends Fragment {
     public void onPause() {
         super.onPause();
         if(player != null){
-            player.setPlayWhenReady(false);
+            videoPosition = player.getCurrentPosition();
+            player.stop();
+            player.release();
+            player=null;
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-       if(player!=null){
-           player.setPlayWhenReady(true);
+       if(player!=null && videoView!=null){
+           player.setPlayWhenReady(getPlayWhenReady);
+       } else if (player==null && videoView!=null){
+           initializePlayer();
        }
     }
 
-
     @Override
     public void onSaveInstanceState(Bundle currentState) {
+        super.onSaveInstanceState(currentState);
         currentState.putString(VIDEO_URL, videoURL);
-        if(player!=null) currentState.putLong(POSITION, getCurrentPosition());
+       if(videoPosition!=null) currentState.putLong(POSITION, videoPosition);
+       currentState.putBoolean(PLAY_STATE, getPlayWhenReady);
     }
 
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {}
 
-    public long getCurrentPosition() {
-        return player.getCurrentPosition();
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {}
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {}
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        getPlayWhenReady = playWhenReady; //The player state will change when user clicks play/pause
     }
 
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {}
+
+    @Override
+    public void onPositionDiscontinuity() {}
 }
